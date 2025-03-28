ca501e8d384a33f42f73cd567a0cbbb401f62ba2
matheus kikuti <matheus.ksousa@sptech.school>
2025-03-28 18:56:03 -0300

    Criando os scripts de criação da VPC, subnets, internet gateway e nat gateway

---
+++ modules/VPC/vpc.tf
@@ -0,0 +1,90 @@
+resource "aws_vpc" "main_vpc" {
+  cidr_block = var.cidr_block
+
+  tags = {
+    Name = var.name
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  tags = {
+    Name = "${var.name}-igw"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id = aws_vpc.main_vpc.id
+    cidr_block = var.public_subnet_cidr
+    map_public_ip_on_launch = true
+
+    tags = {
+      Name = "${var.name}-public-subnet"
+    }
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id     = aws_vpc.main_vpc.id
+  cidr_block = var.private_subnet_cidr
+
+  tags = {
+    Name = "${var.name}-private-subnet"
+  }
+}
+
+resource "aws_eip" "nat_eip" {
+    associate_with_private_ip = null
+    
+    tags = {
+        Name = "${var.name}-nat-eip"
+    }
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public_subnet.id
+
+  tags = {
+    Name = "${var.name}-nat-gateway"
+  }
+}
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  tags = {
+    Name = "${var.name}-public-route-table"
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_association" {
+  subnet_id = aws.subnet.public_subnet.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route" "public_route" {
+  route_table_id = aws_route_table.public_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.igw.id
+}
+
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  tags = {
+    Name = "${var.name}-private-route-table"
+  }
+}
+
+resource "aws_route_table_association" "private_subnet_association" {
+    subnet_id = aws_subnet.private_subnet.id
+    route_table_id = aws_route_table.private_route_table.id
+  
+}
+
+resource "aws_route" "private_route" {
+    route_table_id = aws_route_table.private_route_table.id
+    destination_cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+}
